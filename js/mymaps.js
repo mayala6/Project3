1905cf337479d74a2a98f92cac0095a7e8818225
mayala6 <54589219+mayala6@users.noreply.github.com>
2019-12-09 13:00:19 -0600

    Final upload

--- js/mymaps.js
+++ js/mymaps.js
@@ -1,5 +1,5 @@
 var map;
-var myLocation = { lat: 41.7906, lng: -87.5831 };
+var myLocation = { lat: 41.8623, lng: -87.6167 };
 function initMap() {
  map = new google.maps.Map(document.getElementById("map"), {
     center: myLocation,
@@ -13,11 +13,11 @@ function initMap() {
   var marker = new google.maps.Marker({
     position: myLocation,
     map: map,
-    title: "Museum of Science and Industry",
+    title: "Soldier Field",
     //icon: image
   });
 
-  var info = "The Museum of Science and Industry was originally constructed for the 1893 World's Columbian Exposition's 'White City', originally known as the  Palace of Fine Arts. The museum is the only building still standing from what once was the 'White City'. It was bought and opened by Julius Rosenwald, chairman of Sears, in 1933. He was inspired by a musuem he had seen in Europe and decided to create his own interactive museum. At the time of its opening, it only had one exhibit, the Coal Mine Exhibit which still exists today. Rosenwald had actual coal miners come in and be the tour guides for the exhibit. Now the museum has over 2,000 different exhibits!"
+  var info = "This is Soldier Field, Home of The Bears"
 
   var infowindow = new google.maps.InfoWindow({
     content: info
